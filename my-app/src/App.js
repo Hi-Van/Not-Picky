96b45affb872ff5c60da68f183721ac87e61b5cd
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-07-30 06:15:16 -0400

    Geolocation and Movable Markers
    Google maps can now use user's geolocation and draggable marker

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,155 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
+  InfoWindow,
   withScriptjs,
   withGoogleMap,
   GoogleMap,
   Marker,
 } from "react-google-maps";
+import Geocode from "react-geocode";
+
+Geocode.setApiKey("AIzaSyAZlCCYOnQAZqv6EvGt7Ghtvx4NuXpV0WY");
 
 class App extends React.Component {
+
+  state = {
+    address: "",
+    city: "",
+    area: "",
+    state: "",
+    zoom: 15,
+    height: 400,
+    mapPosition: {
+      lat: 0,
+      lng: 0,
+    },
+    markerPosition: {
+      lat: 0,
+      lng: 0,
+    }
+  }
+
+  getCity = (addressArr) => {
+    let city = '';
+    for (let i = 0; i < addressArr.length; i++) {
+      if (addressArr[i].types[0] && 'administrative_area_level_2' === addressArr[i].types[0]) {
+        city = addressArr[i].long_name;
+        return city;
+      }
+    }
+  }
+
+  getArea = (addressArr) => {
+    let area = '';
+    for (let i = 0; i < addressArr.length; i++) {
+      if (addressArr[i].types[0]) {
+        for (let j = 0; j < addressArr.length; j++) {
+          if (addressArr[i].types[j] === 'sublocality_level_1' || 'locality' === addressArr[i].types[j]) {
+            area = addressArr[i].long_name;
+            return area;
+          }
+        }
+      }
+    }
+  }
+
+  getState = (addressArr) => {
+    let state = '';
+    for (let i = 0; i < addressArr.length; i++) {
+      for (let i = 0; i < addressArr.length; i++) {
+        if (addressArr[i].types[0] && 'administrative_area_level_1' === addressArr[i].types[0]) {
+          state = addressArr[i].long_name;
+          return state;
+        }
+      }
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        mapPosition: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        },
+        markerPosition: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }
+      }, () => {
+        Geocode.fromLatLng(position.coords.latitude, position.coords.longitude)
+          .then(response => {
+
+            const address = response.results[0].formatted_address,
+              addressArr = response.results[0].address_components,
+              city = this.getCity(addressArr),
+              area = this.getArea(addressArr),
+              state = this.getState(addressArr);
+            console.log('response', response);
+            console.log('city', city, area, state);
+            this.setState({
+              address: (address) ? address : '',
+              area: (area) ? area : '',
+              city: (city) ? city : '',
+              state: (state) ? state : ''
+            })
+          })
+      })
+    })
+  }
+
+  onMarkerDragEnd = (event) => {
+    let newLat = event.latLng.lat();
+    let newLng = event.latLng.lng();
+
+    Geocode.fromLatLng(newLat, newLng)
+      .then(response => {
+
+        const address = response.results[0].formatted_address,
+          addressArr = response.results[0].address_components,
+          city = this.getCity(addressArr),
+          area = this.getArea(addressArr),
+          state = this.getState(addressArr);
+        console.log('response', response);
+        console.log('city', city, area, state);
+        this.setState({
+          address: (address) ? address : '',
+          area: (area) ? area : '',
+          city: (city) ? city : '',
+          state: (state) ? state : '',
+          markerPosition: {
+            lat: newLat,
+            lng: newLng
+          },
+          mapPosition: {
+            lat: newLat,
+            lng: newLng
+          }
+        })
+      })
+
+    console.log('newlat', newLat, 'newLng', newLng);
+  }
+
   render() {
 
     const MapWithAMarker = withScriptjs(withGoogleMap(props =>
       <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        defaultZoom={15}
+        defaultCenter={{ lat: this.state.mapPosition.lat, lng: this.state.mapPosition.lng }}
       >
         <Marker
-          position={{ lat: -34.397, lng: 150.644 }}
-        />
+          position={{ lat: this.state.markerPosition.lat, lng: this.state.markerPosition.lng }}
+          draggable={true}
+          onDragEnd={this.onMarkerDragEnd}
+        >
+          <InfoWindow>
+            <div>
+              text
+          </div>
+          </InfoWindow>
+        </Marker>
       </GoogleMap>
     ));
 
